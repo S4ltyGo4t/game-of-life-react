014d364d69da863d2ee31a67f8ccfdf4c47052d7
S4ltyGo4t <mario.weidner@gmx.de>
2020-05-01 11:10:27 +0200

    Updates

---
+++ precache-manifest.bd7b157e77266cd98da9871c2b40b2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42fa21df80c9ec02dd520cd1af733dc",
+    "url": "/game-of-life-react/index.html"
+  },
+  {
+    "revision": "847ec6952441b4d49c13",
+    "url": "/game-of-life-react/static/css/main.ca0dbd98.chunk.css"
+  },
+  {
+    "revision": "595a85f10faed5f8fc09",
+    "url": "/game-of-life-react/static/js/2.89b124e6.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/game-of-life-react/static/js/2.89b124e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "847ec6952441b4d49c13",
+    "url": "/game-of-life-react/static/js/main.7e149664.chunk.js"
+  },
+  {
+    "revision": "c5bf46431e9cdb85f571",
+    "url": "/game-of-life-react/static/js/runtime-main.0c2fa39d.js"
+  }
+]);
